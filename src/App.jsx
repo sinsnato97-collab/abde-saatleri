2c4c7890acf9d3ae362a48f98cc2f2bed801c20d
sinsnato97-collab <sinsnato97@gmail.com>
2026-03-16 17:37:14 +0300

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{color:"white",background:"#111",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <h1>Site çalışıyor</h1>
+    </div>
+  );
+}
